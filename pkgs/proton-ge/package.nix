0c5a836089f0dcb0e335724a2ebca7862a77e9bd
no one <no one>
2025-01-19 17:31:04 +0300

    nothing

--- pkgs/proton-ge/package.nix
+++ pkgs/proton-ge/package.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation (self: {
   pname = "proton-ge";
   version = "9-22";
   src = fetchurl {
-    url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton${self.version}/GE-Proton${self.version}.tar.gz";
+    url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${self.version}/${self.version}.tar.gz";
     hash = "sha256-2H2SPcUm/H/0D9BqBiTFvaCwd0c14/r+oWhyeZdNpoU=";
   };
   buildCommand = ''
