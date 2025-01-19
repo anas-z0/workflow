068eac47f712f143ae272819ab365a906410fce3
nobody <nobody>
2025-01-19 22:43:43 +0300

    fix

--- pkgs/proton-ge/package.nix
+++ pkgs/proton-ge/package.nix
@@ -1,8 +1,8 @@
-{ lib, stdenv, fetchzip, }:
+{ lib, stdenv, fetchurl, }:
 stdenv.mkDerivation rec {
   pname = "proton-ge";
   version = "GE-Proton9-23";
-  src = fetchzip {
+  src = fetchurl {
     url =
       "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
     hash = "sha256-1pWRbn4bjMlwWsR2LIsxFFvEJE4VD8fUIwtSM1MC6I8=";
@@ -11,13 +11,14 @@ stdenv.mkDerivation rec {
   dxvk = stdenv.mkDerivation rec {
     pname = "dxvk";
     version = "1.10.3";
-    src = fetchzip {
+    src = fetchurl {
       url =
         "https://github.com/doitsujin/dxvk/releases/download/v${version}/dxvk-${version}.tar.gz";
       hash = "sha256-jRo8kSdhtFDIefmEeK5k9vZjnkDOaEgXCg9rhZb9U8Y=";
     };
     buildCommand = ''
       runHook preInstall
+      runHook unpackPhase
       mkdir $out
       cp -r ./*/* $out
       runHook postInstall
@@ -25,6 +26,7 @@ stdenv.mkDerivation rec {
   };
   buildCommand = ''
     runHook preInstall
+    runHook unpackPhase
     mkdir $out
     cp -r ./*/* $out/
     ln -sf ${dxvk}/x32/* $out/files/lib/wine/dxvk/
