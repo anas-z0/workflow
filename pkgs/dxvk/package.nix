1af228d42021f34e6d96e1d97af447a2c3725df8
nobody <nobody>
2025-01-19 21:50:00 +0300

    dxvk

---
+++ pkgs/dxvk/package.nix
@@ -0,0 +1,19 @@
+{
+  stdenvNoCC,
+  fetchurl
+}:
+stdenv.mkDerivation (self: {
+  pname = "dxvk";
+  version = "1.10.3";
+  versionWithoutV = builtins.replaceStrings [ "v" ] [ "" ] self.version;
+  src = pkgs.fetchurl {
+    url = "https://github.com/doitsujin/dxvk/releases/download/v${self.version}/dxvk-${self.versionWithoutV}.tar.gz";
+    hash = "sha256-jRo8kSdhtFDIefmEeK5k9vZjnkDOaEgXCg9rhZb9U8Y=";
+  };
+  buildCommand = ''
+    runHook unpackPhase
+    runHook preBuild
+    cp -r ./$name $out
+    runHook postBuild
+  '';
+})
