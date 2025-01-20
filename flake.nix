2cda91939f960860b55f35229334eb78844e6372
nobody <nobody>
2025-01-20 20:32:09 +0300

    test

--- flake.nix
+++ flake.nix
@@ -3,20 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-update = {
+      url = "github:Mic92/nix-update";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: let
-    genSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+  outputs = { self, nixpkgs }@inputs: let
+    genSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ];
     pkgsFor = system: nixpkgs.legacyPackages."${system}";
     in {
       packages = genSystem (system: let pkgs = pkgsFor system; in 
         pkgs.lib.packagesFromDirectoryRecursive { callPackage = pkgs.callPackage; directory = ./pkgs;
       });
-      pinned = {
-        dxvk = "v1.10.3";
+      updateArgs = {
+        proton-ge = "v1.10.3";
         test-but-with-space = "9.1.1 ";
         test = "9.1.1";
       };
+      func = let
+        packages = inputs.nixpkgs.lib.unique (builtins.concatLists (map (x: builtins.attrNames x) (builtins.attrValues self.packages)));
+        argsNames = builtins.attrNames self.updateArgs;
+        args = map (package: if (builtins.elem package argsNames) then builtins.concatStringsSep " " [ "--flake" package self.updateArgs.${package}] else package ) packages;
+      in 
+      test = self;
     };
   }
 
