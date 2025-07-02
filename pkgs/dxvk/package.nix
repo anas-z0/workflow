6fa72bd70542dd7c691077b61686234acd3b7086
nix package updater <nobody>
2025-07-02 13:11:35 +0000

    yes

--- pkgs/dxvk/package.nix
+++ pkgs/dxvk/package.nix
@@ -6,9 +6,8 @@ stdenv.mkDerivation rec {
   pname = "dxvk";
   version = "v1.10.9";
   src = fetchurl {
-    url =
-      "﻿http://github.com/pythonlover02/DXVK-Sarek/releases/download/${version}/dxvk-sarek-async-${version}.tar.gz";
-    #hash = "sha256-jRo8kSdhtFDIefmEeK5k9vZjnkDOaEgXCg9rhZb9U8Y=";
+    url = "http://github.com/pythonlover02/DXVK-Sarek/releases/download/${version}/dxvk-sarek-async-${version}.tar.gz";
+    hash = "sha256-Hb7EEqOMUEdoFUlh/Isc1lnh4GsZ7q0/Atarda50I/Q=";
   };
   buildCommand = ''
     runHook preInstall
