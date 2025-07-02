66594139512e47fdce12d00862f7c0be15bf48eb
nix package updater <nobody>
2025-07-02 10:26:11 +0000

    proton-ge: GE-Proton10-7 -> GE-Proton10-8
    Diff: https://github.com/GloriousEggroll/proton-ge-custom/compare/GE-Proton10-7...GE-Proton10-8

--- pkgs/proton-ge/package.nix
+++ pkgs/proton-ge/package.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, dxvk, }:
 stdenv.mkDerivation rec {
   pname = "proton-ge";
-  version = "GE-Proton10-7";
+  version = "GE-Proton10-8";
   src = fetchurl {
     url =
       "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    hash = "sha256-urfBhG8QNdVmXhDPj8mMPZJ0+3t5Eg6LjufltJfX34o=";
+    hash = "sha256-dgT6q1NWMg7ilHrimBoONZC6KUsVKdSeftA775FOt8g=";
   };
 
   buildCommand = ''
