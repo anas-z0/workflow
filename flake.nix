f9df2b3516577b4d484cee72589a9b53039473e6
AnasSaeed1 <117152551+AnasSaeed1@users.noreply.github.com>
2025-07-07 02:16:58 +0300

    fix fr

--- flake.nix
+++ flake.nix
@@ -1,16 +1,21 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux; lib = nixpkgs.lib;
-    in (x: x (x {})) (self: {
-      packagesNoHash = (x: x (x {})) (self:
-        let
-          callPackage = pkgs.lib.callPackageWith (pkgs // self);
-         in nixpkgs.lib.packagesFromDirectoryRecursive {
-            inherit callPackage;
-            directory = ./pkgs;
-          });
-      packages.x86_64-linux = builtins.mapAttrs (n: v: v.overrideAttrs (x: { src = lib.overrideDerivation packagesNoHash.x86_64-linux.${n}.src (x: { outputHash = (import ./hashes.nix).${n}; }); })) self.packagesNoHash;
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      lib = nixpkgs.lib;
+    in (x: x (x { })) (self: {
+      packagesNoHash = (x: x (x { })) (self:
+        let callPackage = pkgs.lib.callPackageWith (pkgs // self);
+        in nixpkgs.lib.packagesFromDirectoryRecursive {
+          inherit callPackage;
+          directory = ./pkgs;
+        });
+      packages.x86_64-linux = builtins.mapAttrs (n: v:
+        v.overrideAttrs (x: {
+          src = lib.overrideDerivation self.packagesNoHash.${n}.src
+            (x: { outputHash = (import ./hashes.nix).${n}; });
+        })) self.packagesNoHash;
       shell = let
         updateArgs = import ./nix-update.nix;
         packages = nixpkgs.lib.unique (builtins.concatLists
