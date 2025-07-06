c61929a4873f430d469fdd373abb8409671cb523
nix package updater <nobody>
2025-07-06 23:26:52 +0000

    update hashes.nix

--- hashes.nix
+++ hashes.nix
@@ -1,1 +1,1 @@
-{ ani-cli = ""; dxvk = "sha256-Hb7EEqOMUEdoFUlh/Isc1lnh4GsZ7q0/Atarda50I/Q="; proton-ge = "sha256-dgT6q1NWMg7ilHrimBoONZC6KUsVKdSeftA775FOt8g="; }
+{ ani-cli = "sha256-R/YQ02ctTcAEzrVyWlaCHi1YW82iPrMBbbMNP21r0p8="; dxvk = "sha256-Hb7EEqOMUEdoFUlh/Isc1lnh4GsZ7q0/Atarda50I/Q="; proton-ge = "sha256-dgT6q1NWMg7ilHrimBoONZC6KUsVKdSeftA775FOt8g="; }
