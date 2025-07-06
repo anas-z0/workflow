ed82a1e7ea16f785fe6612d297cf9f9bc546fadc
AnasSaeed1 <117152551+AnasSaeed1@users.noreply.github.com>
2025-07-07 02:25:35 +0300

    Update package.nix

--- pkgs/dxvk/package.nix
+++ pkgs/dxvk/package.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
   version = "v1.10.9";
   src = fetchurl {
     url = "http://github.com/pythonlover02/DXVK-Sarek/releases/download/${version}/dxvk-sarek-async-${version}.tar.gz";
-    hash = "sha256-Hb7EEqOMUEdoFUlh/Isc1lnh4GsZ7q0/Atarda50I/Q=";
+    #hash = "sha256-Hb7EEqOMUEdoFUlh/Isc1lnh4GsZ7q0/Atarda50I/Q=";
   };
   buildCommand = ''
     runHook preInstall
