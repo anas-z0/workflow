f287425e133e9a89da18b85a513babae77b65d64
AnasSaeed1 <117152551+AnasSaeed1@users.noreply.github.com>
2025-07-07 02:10:26 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux; lib = nixpkgs.lib;
-    in rec {
+    in (x: x (x {})) (self: {
       packagesNoHash = (x: x (x {})) (self:
         let
           callPackage = pkgs.lib.callPackageWith (pkgs // self);
@@ -10,7 +10,7 @@
             inherit callPackage;
             directory = ./pkgs;
           });
-      packages.x86_64-linux = builtins.mapAttrs (n: v: v.overrideAttrs (x: { src = lib.overrideDerivation packagesNoHash.x86_64-linux.${n}.src (x: { outputHash = (import ./hashes.nix).${n}; }); })) inputs.self.outputs.packagesNoHash;
+      packages.x86_64-linux = builtins.mapAttrs (n: v: v.overrideAttrs (x: { src = lib.overrideDerivation packagesNoHash.x86_64-linux.${n}.src (x: { outputHash = (import ./hashes.nix).${n}; }); })) self.outputs.packagesNoHash;
       shell = let
         updateArgs = import ./nix-update.nix;
         packages = nixpkgs.lib.unique (builtins.concatLists
@@ -23,5 +23,5 @@
           else
             package) + " 1>/dev/null") packages;
       in builtins.concatStringsSep "\n" list;
-    };
+    });
 }
