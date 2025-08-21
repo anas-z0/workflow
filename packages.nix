8114a145f42641904c68ee6c357cce6e9b55c589
AnasSaeed1 <117152551+AnasSaeed1@users.noreply.github.com>
2025-08-21 11:41:07 +0300

    Update packages.nix

--- packages.nix
+++ packages.nix
@@ -1,5 +1,3 @@
 {
-  "GloriousEggroll/proton-ge-custom" = { ver = "GE-Proton10-12"; url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-12/GE-Proton10-12.tar.gz"; };
-  "an-anime-team/an-anime-game-launcher" = { ver = "3.15.5"; url = "https://github.com/an-anime-team/an-anime-game-launcher/releases/download/3.15.6/anime-game-launcher"; };
   "pythonlover02/DXVK-Sarek" = { ver = "v1.11.0"; url = "https://github.com/pythonlover02/DXVK-Sarek/releases/download/v1.11.0/dxvk-sarek-async-v1.11.0.tar.gz"; };
 }
