38dbe6bcf54463af8a24f2546ac129d98966b030
nix package updater <nobody>
2025-08-21 07:42:20 +0000

    update hashes.nix packages.nix

--- deps.nix
+++ deps.nix
@@ -1,6 +1,8 @@
 {
 "GloriousEggroll/proton-ge-custom" = [
 ];
+"an-anime-team/an-anime-game-launcher" = [
+];
 "pythonlover02/DXVK-Sarek" = [
 ];
 }
--- packages.nix
+++ packages.nix
@@ -1,5 +1,5 @@
 {
-  "GloriousEggroll/proton-ge-custom" = { ver = "GE-Proton10-8"; url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-8/GE-Proton10-8.tar.gz"; };
-  "an-anime-team/an-anime-game-launcher" = { ver = "3.14.3"; url = "https://github.com/an-anime-team/an-anime-game-launcher/releases/download/3.14.3/anime-game-launcher"; };
-  "pythonlover02/DXVK-Sarek" = { ver = "v1.10.9"; url = "https://github.com/pythonlover02/DXVK-Sarek/releases/download/v1.10.9/dxvk-sarek-async-v1.10.9.tar.gz"; };
+  "GloriousEggroll/proton-ge-custom" = { ver = "GE-Proton10-12"; url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-12/GE-Proton10-12.tar.gz"; };
+  "an-anime-team/an-anime-game-launcher" = { ver = "3.15.6"; url = "https://github.com/an-anime-team/an-anime-game-launcher/releases/download/3.15.6/anime-game-launcher"; };
+  "pythonlover02/DXVK-Sarek" = { ver = "v1.11.0"; url = "https://github.com/pythonlover02/DXVK-Sarek/releases/download/v1.11.0/dxvk-sarek-async-v1.11.0.tar.gz"; };
 }
