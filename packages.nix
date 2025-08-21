c4527996819cf0c995c776fdb17024856d98af47
nix package updater <nobody>
2025-08-21 09:12:59 +0000

    update packages.nix

--- packages.nix
+++ packages.nix
@@ -1,17 +1,4 @@
 {
-  "GloriousEggroll/proton-ge-custom" = {
-    ver = "GE-Proton10-12";
-    url =
-      "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-12/GE-Proton10-12.tar.gz";
-  };
-  "an-anime-team/an-anime-game-launcher" = {
-    ver = "3.15.6";
-    url =
-      "https://github.com/an-anime-team/an-anime-game-launcher/releases/download/3.15.6/anime-game-launcher";
-  };
-  "pythonlover02/DXVK-Sarek" = {
-    ver = "v1.11.0";
-    url =
-      "https://github.com/pythonlover02/DXVK-Sarek/releases/download/v1.11.0/dxvk-sarek-async-v1.11.0.tar.gz";
-  };
+  "GloriousEggroll/proton-ge-custom" = { version = "GE-Proton10-12"; url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-12/GE-Proton10-12.tar.gz"; };
+  "pythonlover02/DXVK-Sarek" = { version = "v1.11.0"; url = "https://github.com/pythonlover02/DXVK-Sarek/releases/download/v1.11.0/dxvk-sarek-async-v1.11.0.tar.gz"; };
 }
