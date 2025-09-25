91a068bd2a20fc6e35b82d7ce1eaae885aa05a2f
nix package updater <nobody>
2025-09-25 01:43:28 +0000

    update hashes.nix

--- hashes.nix
+++ hashes.nix
@@ -1,1 +1,1 @@
-{ dxvk = "sha256-szFvHgVJMBn1BzLdEo4xXyc/g5XT6c/gI6QmPlpdG5M="; proton-ge = "sha256-X231hYZUtV8PgsInjhOpallmwldqwTqSVCKtLT5worQ="; }
+{ dxvk = "sha256-szFvHgVJMBn1BzLdEo4xXyc/g5XT6c/gI6QmPlpdG5M="; proton-ge = "sha256-SgMG0tNfweIubdgPepiC0ZB3N2gjOeIj+DhAhQ5PMUE="; }
