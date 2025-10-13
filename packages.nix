00990c4c6fd7b25975a6f7e4800c4d6b0a05706f
nix package updater <nobody>
2025-10-13 01:49:43 +0000

    update packages.nix

--- packages.nix
+++ packages.nix
@@ -1,4 +1,4 @@
 {
-  "GloriousEggroll/proton-ge-custom" = { version = "GE-Proton10-17"; url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-17/GE-Proton10-17.tar.gz"; };
+  "GloriousEggroll/proton-ge-custom" = { version = "GE-Proton10-18"; url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-18/GE-Proton10-18.tar.gz"; };
   "pythonlover02/DXVK-Sarek" = { version = "v1.11.0"; url = "https://github.com/pythonlover02/DXVK-Sarek/releases/download/v1.11.0/dxvk-sarek-async-v1.11.0.tar.gz"; };
 }
