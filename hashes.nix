912dcf62c3ac0d660ecba1cd7f2b1e9e87af3b7b
nix package updater <nobody>
2025-10-21 01:47:13 +0000

    update hashes.nix

--- hashes.nix
+++ hashes.nix
@@ -1,1 +1,1 @@
-{ dxvk = "sha256-szFvHgVJMBn1BzLdEo4xXyc/g5XT6c/gI6QmPlpdG5M="; proton-ge = "sha256-IUvsEZmG4cqzy606Keh6NQbCI5RJr0mxS0EEcJanfoA="; }
+{ dxvk = "sha256-szFvHgVJMBn1BzLdEo4xXyc/g5XT6c/gI6QmPlpdG5M="; proton-ge = "sha256-vMBTKQ38uUzT+I3cZ9lssEIaCNqOxIVUz4qfKH9ptAs="; }
