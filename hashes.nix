f731afd378666818c02521f37669119d38323935
nix package updater <nobody>
2025-11-03 01:54:17 +0000

    update hashes.nix

--- hashes.nix
+++ hashes.nix
@@ -1,1 +1,1 @@
-{ dxvk = "sha256-szFvHgVJMBn1BzLdEo4xXyc/g5XT6c/gI6QmPlpdG5M="; proton-ge = "sha256-vUHwXVOPv0gGC04XUQQFZEVPfG47uqgn/AKXuVKy7eU="; }
+{ dxvk = ""; proton-ge = "sha256-sOSnHNimEQoiKTTVUSclEQwOO3gPXyk9EnptHrnIum0="; }
