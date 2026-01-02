44f66a9abaaa759fe43fd3970779e11e34264b22
nix package updater <nobody>
2026-01-02 02:01:35 +0000

    update hashes.nix

--- hashes.nix
+++ hashes.nix
@@ -1,1 +1,1 @@
-{ dxvk = "sha256-szFvHgVJMBn1BzLdEo4xXyc/g5XT6c/gI6QmPlpdG5M="; proton-ge = "sha256-pNl47febboYRprWlM3FFujK4ZmcAScGYzXOxW9KdVKk="; }
+{ dxvk = "sha256-szFvHgVJMBn1BzLdEo4xXyc/g5XT6c/gI6QmPlpdG5M="; proton-ge = ""; }
