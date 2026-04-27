e0c09988fd20bb44c788a822c57d0db980414082
nix package updater <nobody>
2026-04-27 03:30:47 +0000

    update hashes.nix

--- hashes.nix
+++ hashes.nix
@@ -1,1 +1,1 @@
-{ dxvk = "sha256-kcuOaqzE21mDp+Cm1EKNRTBiGDDp8xtO+ZYC6zDrsi8="; proton-ge = "sha256-UcWAtmqDPHOZj+APBxfurFcZdlQECi8u1RiePuaNdz0="; }
+{ dxvk = ""; proton-ge = "sha256-UcWAtmqDPHOZj+APBxfurFcZdlQECi8u1RiePuaNdz0="; }
